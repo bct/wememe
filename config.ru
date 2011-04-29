c9b659062637a5f0dbc13641cfb08c108e072ec4
Brendan Taylor <whateley@gmail.com>
2011-04-28 21:36:16 -0600

    added an index and a login page

--- config.ru
+++ config.ru
@@ -7,17 +7,4 @@ require "#{this_dir}/rememe"
 
 ENV['RACK_ENV'] ||= "development"
 
-rack_env = ENV['RACK_ENV']
-
-unless rack_env == "production"
-  config = YAML.load_file(File.join("#{this_dir}/config.yml"))[rack_env]
-
-  config.each do |key, value|
-    ENV[key] = value
-  end
-else
-  require 'exceptional'
-  use Rack::Exceptional, ENV['EXCEPTIONAL_KEY']
-end
-
 run Rememe
--- rememe.rb
+++ rememe.rb
@@ -1,7 +1,21 @@
 require 'bundler'
 Bundler.require
 
+require_relative 'models'
+
 class Rememe < Sinatra::Base
+  configure do
+    set :db, "sqlite3:///#{Dir.pwd}/db/wememe.db"
+  end
+
+  get '/' do
+    haml :index
+  end
+
+  get '/login' do
+    haml :login
+  end
+
   get '/dev/styles' do
     haml :styles
   end
@@ -11,3 +25,7 @@ class Rememe < Sinatra::Base
     scss :screen, Compass.sass_engine_options
   end
 end
+
+#DataObjects::Sqlite3.logger = DataMapper::Logger.new(STDOUT, :debug)
+
+DataMapper.setup :default, Rememe.db
